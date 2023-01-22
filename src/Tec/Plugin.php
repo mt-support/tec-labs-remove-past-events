3dd3ae098803f8c9e1d8df36e8385b6779acdc22
András Guseo <andras@theeventscalendar.com>
2023-01-22 22:59:06 +0100

    Added the original filter back to keep compatibility with older versions

--- src/Tec/Plugin.php
+++ src/Tec/Plugin.php
@@ -92,6 +92,7 @@ class Plugin extends \tad_DI52_ServiceProvider {
 		// Start binds.
 
 		add_filter( 'tribe_events_delete_old_events_sql', [ $this, 'cleanup_query' ] );
+		add_filter( 'tribe-event-general-settings-fields', [ $this, 'option_filter' ] );
 		add_filter( 'tribe_general_settings_tab_fields', [ $this, 'option_filter' ] );
 		add_action( 'plugins_loaded', [ $this, 'reschedule_crons' ], 99 );
 
